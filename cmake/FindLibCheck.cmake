ea5055bbc9e2b14e046b204e144e39f9c65df7da
Callum C <callumc34@hotmail.com>
2023-08-08 20:36:08 +1200

    feat: Added build documentation options to cmake

--- cmake/FindLibCheck.cmake
+++ cmake/FindLibCheck.cmake
@@ -1,3 +1,6 @@
+find_package(Doxygen)
+
+# Add user defined Qt6 install prefix to CMAKE_PREFIX_PATH for find_package to work
 list(APPEND CMAKE_PREFIX_PATH ${Qt6_INSTALL_PREFIX})
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Widgets)
+find_package(Qt6 6.3.2 REQUIRED COMPONENTS Core Gui Widgets)
 qt_standard_project_setup()
