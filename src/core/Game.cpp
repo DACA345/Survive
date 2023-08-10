5b6d9b8e6d860fc84987fc9bfd044b92ea0dba06
Drujehong <drujehong@gmail.com>
2023-08-10 17:27:08 +1200

    Guidelines for @dalee371
    I've provided the rough pseudocode for a minimum playable experience in Game.cpp

---
+++ src/core/Game.cpp
@@ -0,0 +1,44 @@
+// Initialize game variables
+//int currentDay = 1;
+//bool gameOver = false;
+
+// Main game loop
+//while (!gameOver) {
+    // Print current day and available turns
+    //print("Day " + currentDay);
+    //int remainingTurns = 5;
+
+    // Perform player's actions for the day
+    //while (remainingTurns > 0) {
+        // Display available actions and prompt player to choose an action
+    //    displayAvailableActions();
+    //    Action chosenAction = getPlayerChoice();
+
+        // Apply effects of the chosen action to the bars
+    //    energyBar.applyEffects(chosenAction);
+    //    hungerBar.applyEffects(chosenAction);
+    //    thirstBar.applyEffects(chosenAction);
+
+        // Decrement remaining turns
+    //    remainingTurns--;
+    //}
+
+    // Check for random events and apply effects
+    //if (randomEventOccurs()) {
+    //    Event randomEvent = getRandomEvent();
+    //    energyBar.applyEffects(randomEvent);
+    //    hungerBar.applyEffects(randomEvent);
+    //    thirstBar.applyEffects(randomEvent);
+    //}
+
+    // Check if any of the bars have reached zero
+    // if (energyBar.isZero() || hungerBar.isZero() || thirstBar.isZero()) {
+        // Game over
+    //} 
+    //else {
+        // Proceed to the next day
+        
+    //}
+//}
+
+// End of game loop
