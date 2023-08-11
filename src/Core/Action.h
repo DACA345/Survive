2fd7afa9aaee767875d50939c3cf31c1a4966865
leeda371 <leeda371@student.otago.ac.nz>
2023-08-11 16:36:22 +1200

    Include the methods for action class

--- src/Core/Action.h
+++ src/Core/Action.h
@@ -1,2 +1,23 @@
 #include <QString>
 #include <QMap>
+
+class Action {
+public:
+    // Constructor
+    Action(const QString& filePath);
+
+    // Load method
+    void loadActionsFromJson(const QString& filePath);
+
+    // Print method
+    void printAllActions() const;
+
+    // Get random method 
+    QString getRandomAction() const;
+
+    // Print random method
+    void printRandomAction() const;
+
+private:
+    QMap<QString, QString> actions;
+};
