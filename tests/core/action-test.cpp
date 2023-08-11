ac01946ac15edd65072ad58fed9eb972e7341e04
leeda371 <leeda371@student.otago.ac.nz>
2023-08-11 21:51:27 +1200

    Updated action json file

---
+++ tests/core/action-test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <Core/Action.h>
+
+TEST(ActionTest, MethodConstructorTest)
+{
+    // Test getting a random action
+    Action action("data/levels/mtaspiring/actions.json");
+
+    // Test getting a random action from a specific category
+    QString randomAction = action.getAction("morale");  // Change "morale" to the desired category
+
+    // Check if the returned action description is not empty
+    ASSERT_FALSE(randomAction.isEmpty()) << "Random action is empty.";
+
+    // Print the randomly selected action description
+    std::cout << "Random action: " << randomAction.toStdString() << std::endl;
+}
