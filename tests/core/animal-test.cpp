ac03b06f3f48b494affacfb1002b4a2a4c418861
Drujehong <drujehong@gmail.com>
2023-08-12 22:52:46 +1200

    Added test statements and also Game.h file

--- tests/core/animal-test.cpp
+++ tests/core/animal-test.cpp
@@ -6,7 +6,7 @@ TEST(AnimalTests, MethodConstructorTest)
     // Test getting a random bird
     Animal animal("data/levels/mtaspiring/animals.json");
 
-    QString randomAnimal = animal.getRandomAnimal("mammals");
+    QString randomAnimal = animal.getRandomAnimal("birds");
     std::cout << "Random animal: " << randomAnimal.toStdString() << std::endl;
 
 }
