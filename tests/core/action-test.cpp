93f67ed9db3aaefd218c8e3002f41d8af847ba5b
leeda371 <leeda371@student.otago.ac.nz>
2023-08-17 21:29:04 +1200

    update test class for action and animal that reflect the memory efficiency.

--- tests/core/action-test.cpp
+++ tests/core/action-test.cpp
@@ -3,12 +3,14 @@
 
 TEST(ActionTest, MethodConstructorTest)
 {
-    // Test getting a random action
-    Action action("data/levels/mtaspiring/actions.json");
+    Action testAction("data/levels/mtaspiring/actions.json");
 
-    // Test getting a random action from a specific category
-    QString randomAction = action.getRandomAction("morale");  // Change "morale" to the desired category
+    // Test for the "Fungi" category
+    EXPECT_NO_THROW(testAction.getRandomActionCategory());
+    EXPECT_NO_THROW(testAction.getRandomAction());
+    EXPECT_NO_THROW(testAction.getRandomActionInCategory("morale"));
+    EXPECT_NO_THROW(testAction.getRandomActionInCategory("water"));
 
-    // Print the randomly selected action description
-    std::cout << "Random action: " << randomAction.toStdString() << std::endl;
+    ASSERT_FALSE(testAction.getRandomAction().actionsID == "");
+    ASSERT_FALSE(testAction.getRandomAction().category == "");
 }
--- tests/core/animal-test.cpp
+++ tests/core/animal-test.cpp
@@ -3,10 +3,15 @@
 
 TEST(AnimalTest, MethodConstructorTest)
 {
-    // Test getting a random bird
-    Animal animal("data/levels/mtaspiring/animals.json");
+    Animal testAnimal("data/levels/mtaspiring/animals.json");
 
-    QString randomAnimal = animal.getRandomAnimal("birds");
-    std::cout << "Random animal: " << randomAnimal.toStdString() << std::endl;
+    // Test for the "birds" category
+    EXPECT_NO_THROW(testAnimal.getRandomAnimalCategory());
+    EXPECT_NO_THROW(testAnimal.getRandomAnimal());
+    EXPECT_NO_THROW(testAnimal.getRandomAnimalInCategory("birds"));
+    EXPECT_NO_THROW(testAnimal.getRandomAnimalInCategory("mammals"));
+
+    ASSERT_FALSE(testAnimal.getRandomAnimal().name == "");
+    ASSERT_FALSE(testAnimal.getRandomAnimal().category == "");
 
 }
